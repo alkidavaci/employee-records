19f6fb94bc93ae8b39170b7f9ce7ed8d9f3af875
Alkida Vaci <alkida.vaci@gmail.com>
2022-12-16 20:09:32 -0500

    Added schema file.  The logical structures of data.

---
+++ db/schema.sql
@@ -0,0 +1,38 @@
+-- Drop and create database
+DROP DATABASE IF EXISTS employees_db;
+
+CREATE DATABASE employees_db;
+
+--Use database
+USE employees_db;
+
+-- Create table departments
+CREATE TABLE departments (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL
+);
+
+-- Create table role
+CREATE TABLE role (
+    id INT  PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(100) NOT NULL,
+    salary DECIMAL(10,2) NULL,
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES departments(id)
+);
+
+-- Create table employee
+CREATE TABLE employee (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE SET NULL,
+    manager_id INT,
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+    ON DELETE SET NULL
+);
+
