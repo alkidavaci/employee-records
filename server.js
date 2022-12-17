7d6fba9ae06f7240f761a7a1b460a5c0ece93c66
Alkida Vaci <alkida.vaci@gmail.com>
2022-12-17 00:19:54 -0500

    Display deparments table

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const inquirer = require("inquirer");
 const express = require('express');
 const mysql = require('mysql2');
 const db = require('./config/connection');
+const cTable = require('console.table')
 
 // Initial port
 const PORT = process.env.PORT || 3001;
@@ -15,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //! THEN I am presented with the following options: view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
-// THEN I am presented with a formatted table showing department names and department ids
+//! THEN I am presented with a formatted table showing department names and department ids
 // THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
 // THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
 // THEN I am prompted to enter the name of the department and that department is added to the database
@@ -39,9 +40,9 @@ function runChoices() {
         message: "What would you like to do?",
         name: "choice",
         choices: [
-                "View All Employees", 
                 "View All Departments",
                 "View All Roles",
+                "View All Employees", 
                 "View All Employees by Department",
                 "View All Employees by Role",
                 "Add Department",
@@ -54,16 +55,17 @@ function runChoices() {
         .then((answer) => {
             switch (answer.choice) {
 
-                case "View All Employees":
-                    console.log(answer.choice);
-                break;
-
                 case "View All Departments":
-                    console.log(answer.choice);
+                    console.log("\n", answer.choice, "\n");
+                    allDepartments();
                 break;
-
+                
                 case  "View All Roles":
                     console.log(answer.choice);
+                break;
+
+                case "View All Employees":
+                    console.log(answer.choice);                    
                 break;
 
                 case "View All Employees by Department":
@@ -95,4 +97,17 @@ function runChoices() {
                 break;
             };
         });
+};
+
+// Function view all departments
+function allDepartments(){
+    db.query("SELECT departments.id AS ID, departments.name AS Department FROM departments", 
+
+    function (err, res) {
+        if (err) throw err;
+        // Display json data in a table
+        console.table(res);
+        // Run the choices 
+        runChoices();
+      });   
 };
